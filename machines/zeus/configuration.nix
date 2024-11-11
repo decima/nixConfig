317fd15c26db46ac91e342582e122d9f4ea9129e
decima <h.larget@gmail.com>
2024-11-11 22:25:21 +0100

    app(steam): adding default config and steam

--- machines/zeus/configuration.nix
+++ machines/zeus/configuration.nix
@@ -13,6 +13,12 @@
         discord-ptb
     ];
 
+    programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
     # #configure swapfiles and other machine specific configuration here: 
     # swapDevices = [{
     #     device = "/swapfile";
