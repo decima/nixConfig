51bab3a6b72cf5f7fa117929b8960d2f511f5d00
decima <h.larget@gmail.com>
2024-11-11 11:51:57 +0100

    app(hyprland): add cachix

--- configuration.nix
+++ configuration.nix
@@ -134,5 +134,8 @@
 
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
-
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 }
--- flake.nix
+++ flake.nix
@@ -3,8 +3,12 @@
 
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-24.05";
-        home-manager.url = "github:nix-community/home-manager/release-24.05";
-        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+        home-manager = {
+            url = "github:nix-community/home-manager/release-24.05";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        hyprland.url = "github:hyprwm/Hyprland";
+
     };
 
     outputs = { self, nixpkgs, home-manager, ...}:
@@ -12,6 +16,33 @@
             lib = nixpkgs.lib;
             system = "x86_64-linux";
             pkgs = nixpkgs.legacyPackages.${system};
+            hyprlandModule = {
+                wayland.windowManager.hyprland = {
+                    enable = true;
+                    # set the flake package
+                    package = self.inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+                    settings = {
+                        "$mod" = "SUPER";
+                        bind =
+                        [
+                            "$mod, F, exec, firefox"
+                            ", Print, exec, grimblast copy area"
+                        ]
+                        ++ (
+                            # workspaces
+                            # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+                            builtins.concatLists (builtins.genList (i:
+                                let ws = i + 1;
+                                in [
+                                "$mod, code:1${toString i}, workspace, ${toString ws}"
+                                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+                                ]
+                            )
+                            9)
+                        );
+                    };
+                };
+            };
         in {
 
 
@@ -20,7 +51,10 @@
             homeConfigurations = {
                 decima = home-manager.lib.homeManagerConfiguration {
                     inherit pkgs;
-                    modules = [./homes/decima.nix];
+                    modules = [
+                        hyprlandModule
+                        ./homes/decima.nix
+                    ];
                 };
             };
         
