74cdbe1d8feb695daa24964446733b93b3fa59cb
decima <h.larget@gmail.com>
2024-11-11 19:31:15 +0100

    fix(vm): move grub bootloader to vm

--- machines/vm/configuration.nix
+++ machines/vm/configuration.nix
@@ -6,6 +6,10 @@
         ../../configuration.nix
     ];
     networking.hostName = "vm"; # Define your hostname.
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sda";
+    boot.loader.grub.useOSProber = true;
+
     swapDevices = [{
         device = "/swapfile";
         size = 2 * 1024; # 16GB
